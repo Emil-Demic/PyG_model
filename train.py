248925363949ffc5c14d4fcf34e01913e1c6a45e
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-08-15 18:18:35 +0200

    plz 2

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ loader_image_test = DataLoader(dataset_image_test, batch_size=args.batch_size * 
 model = TripletModel()
 model.cuda()
 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-scheduler = lr_scheduler.StepLR(optimizer, 10, gamma=0.1, last_epoch=-1)
+scheduler = lr_scheduler.StepLR(optimizer, 5, gamma=0.1, last_epoch=-1)
 loss = TripletMarginLoss()
 loss.cuda()
 
