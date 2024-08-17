52f80650eaa11a48e1063ef3f08673d4b00faa7f
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-08-17 17:28:45 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import Sequential, Identity, Linear
+from torch.nn import Sequential, Identity, Linear, Parameter
 import torch.nn.functional as F
 from torch_geometric.nn.dense import DenseGATConv, DenseSAGEConv
 from torch_geometric.utils import to_dense_batch
@@ -10,8 +10,8 @@ from torchvision.ops import roi_align
 class Model1(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        # self.pool_W1 = Linear(in_features=1024, out_features=512)
-        # self.pool_W2 = Linear(in_features=1024, out_features=512)
+        self.pool_W1 = Linear(in_features=1024, out_features=512)
+        self.pool_W2 = Linear(in_features=1024, out_features=512)
         self.conv1 = DenseGATConv(4096, 512, heads=2)
         model_s = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.DEFAULT)
         self.feature_extractor_sketch = Sequential(*(list(model_s.children())[:-2]))
@@ -28,14 +28,17 @@ class Model1(torch.nn.Module):
         x = roi_align(extracted_features, x, spatial_scale=7. / 224., output_size=1)
         x = x.squeeze((2, 3))
         global_features = global_features.squeeze((2, 3))
-        global_features = torch.repeat_interleave(global_features, torch.bincount(batch), dim=0)
+        bincount = torch.bincount(batch)
+        global_features = torch.repeat_interleave(global_features, bincount, dim=0)
         x = torch.concat((x, global_features), dim=1)
         x = to_dense_batch(x, batch)
-        count = torch.count_nonzero(x[1], dim=1).to(torch.float)
+        non_zero = x[1].to(torch.int32).unsqueeze(2)
+        # count = torch.count_nonzero(x[1], dim=1).to(torch.float)
         x = self.conv1(x[0], edge_index)
-        # x = F.sigmoid(self.pool_W1(x)) * self.pool_W2(x)
+        x = F.sigmoid(self.pool_W1(x)) * (self.pool_W2(x))
+        x = x * non_zero
         x = torch.sum(x, dim=1, keepdim=False)
-        x = x / count.unsqueeze(1)
+        # x = x / count.unsqueeze(1)
         # x = torch.mean(x, dim=1)
         return x
 
