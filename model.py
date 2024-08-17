dc95f47b2b5e3567a34cb2057e966c312b1f8272
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-08-17 16:07:52 +0200

    Update model.py

--- model.py
+++ model.py
@@ -25,7 +25,7 @@ class Model1(torch.nn.Module):
         else:
             extracted_features = self.feature_extractor_image(img)
         global_features = self.global_pool(extracted_features)
-        boxes = x[:, 1:]
+        boxes = x[:, 1:] / 224.
         x = roi_align(extracted_features, x, spatial_scale=7. / 224., output_size=1)
         x = x.squeeze((2, 3))
         global_features = global_features.squeeze((2, 3))
