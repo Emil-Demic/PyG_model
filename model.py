ccf34fe997bc57d3c3ee8887a706642ec401e624
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-08-17 16:21:18 +0200

    Update model.py

--- model.py
+++ model.py
@@ -10,8 +10,8 @@ from torchvision.ops import roi_align
 class Model1(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.pool_W1 = Linear(in_features=512, out_features=512)
-        self.pool_W2 = Linear(in_features=512, out_features=512)
+        self.pool_W1 = Linear(in_features=1024, out_features=512)
+        self.pool_W2 = Linear(in_features=1024, out_features=512)
         self.conv1 = DenseGATConv(4096, 512, heads=2)
         model_s = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.DEFAULT)
         self.feature_extractor_sketch = Sequential(*(list(model_s.children())[:-2]))
