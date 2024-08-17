a1bdb290ab6d464974611914ecf399d9ec3154a6
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-08-17 17:38:33 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 from torch.nn import Sequential, Identity, Linear, Parameter
 import torch.nn.functional as F
-from torch_geometric.nn.dense import DenseGATConv, DenseSAGEConv
+from torch_geometric.nn.dense import DenseGATConv, DenseSAGEConv, DenseGraphConv
 from torch_geometric.utils import to_dense_batch
 from torchvision.models import ResNeXt50_32X4D_Weights, resnext50_32x4d
 from torchvision.ops import roi_align
@@ -12,7 +12,7 @@ class Model1(torch.nn.Module):
         super().__init__()
         self.pool_W1 = Linear(in_features=512, out_features=512)
         self.pool_W2 = Linear(in_features=512, out_features=512)
-        self.conv1 = DenseSAGEConv(4096, 512)
+        self.conv1 = DenseGraphConv(4096, 512)
         model_s = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.DEFAULT)
         self.feature_extractor_sketch = Sequential(*(list(model_s.children())[:-2]))
         model_i = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.DEFAULT)
@@ -33,12 +33,12 @@ class Model1(torch.nn.Module):
         x = torch.concat((x, global_features), dim=1)
         x = to_dense_batch(x, batch)
         non_zero = x[1].to(torch.int32).unsqueeze(2)
-        count = torch.count_nonzero(x[1], dim=1).to(torch.float)
+        # count = torch.count_nonzero(x[1], dim=1).to(torch.float)
         x = self.conv1(x[0], edge_index)
         x = x * non_zero
         x = F.sigmoid(self.pool_W1(x)) * (self.pool_W2(x))
         x = torch.sum(x, dim=1, keepdim=False)
-        x = x / count.unsqueeze(1)
+        # x = x / count.unsqueeze(1)
         # x = torch.mean(x, dim=1)
         return x
 
