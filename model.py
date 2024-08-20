0ea34f5be432d2f844902e6ed85ed5d507c257be
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-08-20 12:28:56 +0200

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from torch_geometric.nn.dense import DenseGATConv
 from torch_geometric.nn.conv import TransformerConv, GATConv
 from torch_geometric.utils import to_dense_batch
-from torchvision.models import resnext50_32x4d, ResNeXt50_32X4D_Weights, convnext_tiny, ConvNeXt_Tiny_Weights
+from torchvision.models import resnext50_32x4d, ResNeXt50_32X4D_Weights
 from torchvision.ops import roi_align
 
 from layer import GNNLayer
@@ -19,8 +19,7 @@ class Model1(torch.nn.Module):
         # self.fc_cls_in = Linear(in_features=4096, out_features=91)
         # self.fc_det_out = Linear(in_features=512, out_features=91)
         # self.fc_cls_out = Linear(in_features=512, out_features=91)
-        # self.conv1 = GATConv(2048, 512, heads=2)
-        self.conv1 = GNNLayer(4096, 512)
+        self.conv1 = DenseGATConv(4096, 512)
         model_s = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.DEFAULT)
         self.feature_extractor_sketch = Sequential(*(list(model_s.children())[:-2]))
         model_i = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.DEFAULT)
@@ -40,13 +39,12 @@ class Model1(torch.nn.Module):
         x = torch.concat((x, global_features), dim=1)
         x = to_dense_batch(x, batch)
         non_zero = x[1].to(torch.int32).unsqueeze(2)
-        # count = torch.count_nonzero(x[1], dim=1).to(torch.float)
+        count = torch.count_nonzero(x[1], dim=1).to(torch.float)
         x = self.conv1(x[0], edge_index)
         x = x * non_zero
         x = F.sigmoid(self.pool_W1(x)) * (self.pool_W2(x))
-        # x = x * non_zero
         x = torch.sum(x, dim=1, keepdim=False)
-        # x = x / count.unsqueeze(1)
+        x = x / count.unsqueeze(1)
         # x = torch.mean(x, dim=1)
         return x
 
