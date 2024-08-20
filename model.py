8ba06b7c4e29f0c214ccecd58d007fbe06797d6d
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-08-20 12:05:08 +0200

    Update model.py

--- model.py
+++ model.py
@@ -20,7 +20,7 @@ class Model1(torch.nn.Module):
         # self.fc_det_out = Linear(in_features=512, out_features=91)
         # self.fc_cls_out = Linear(in_features=512, out_features=91)
         # self.conv1 = GATConv(2048, 512, heads=2)
-        self.conv1 = GNNLayer(1536, 512)
+        self.conv1 = GNNLayer(4096, 512)
         model_s = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.DEFAULT)
         self.feature_extractor_sketch = Sequential(*(list(model_s.children())[:-2]))
         model_i = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.DEFAULT)
