ecd91aea7071cfaf61c6d751a601ceee79688c7a
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-08-20 13:29:44 +0200

    Update model.py

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ class Model1(torch.nn.Module):
 
     def forward(self, x, edge_index, img, batch, sketch=True):
         x = self.feature_extractor(img)
-        x = self.pool_method(x).view(-1, 768)
+        x = self.pool_method(x).view(-1, 1024)
         return F.normalize(x)
 
 
